6d79a5ca35cd49e3749c33d6ee819bb14073d443
pengfei_allen_xiao <pengfei.allen.xiao@singulariti.co>
2019-10-18 14:34:37 +1100

    streaming section finished

--- 5-Spark Streaming/TweetRead.py
+++ 5-Spark Streaming/TweetRead.py
@@ -5,51 +5,52 @@ from tweepy.streaming import StreamListener
 import socket
 import json
 
-
 # Set up your credentials
-consumer_key=''
-consumer_secret=''
-access_token =''
-access_secret=''
+consumer_key = 'YZXvTb6qbwRuPflCS6gUDuxnL'
+consumer_secret = 'uZi7W0uUEfCGi4vwlkruX0JSe8JZV47UH5AFHptU0JYdPpJTZQ'
+access_token = '967540920290754560-IWECpltxXhsZGUCQLPAGH1xIXds2TEz'
+access_secret = 'l96uRBEq4s2syHCCvmtGBooe1XqsMZT7Jo2R0D1u9WeqO'
 
 
 class TweetsListener(StreamListener):
 
-  def __init__(self, csocket):
-      self.client_socket = csocket
+    def __init__(self, csocket):
+        self.client_socket = csocket
 
-  def on_data(self, data):
-      try:
-          msg = json.loads( data )
-          print( msg['text'].encode('utf-8') )
-          self.client_socket.send( msg['text'].encode('utf-8') )
-          return True
-      except BaseException as e:
-          print("Error on_data: %s" % str(e))
-      return True
+    def on_data(self, data):
+        try:
+            msg = json.loads(data)
+            print(msg['text'].encode('utf-8'))
+            self.client_socket.send(msg['text'].encode('utf-8'))
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
 
-  def on_error(self, status):
-      print(status)
-      return True
+    def on_error(self, status):
+        print(status)
+        return True
+
 
 def sendData(c_socket):
-  auth = OAuthHandler(consumer_key, consumer_secret)
-  auth.set_access_token(access_token, access_secret)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
 
-  twitter_stream = Stream(auth, TweetsListener(c_socket))
-  twitter_stream.filter(track=['soccer'])
+    twitter_stream = Stream(auth, TweetsListener(c_socket))
+    twitter_stream.filter(track=['python'])
+
 
 if __name__ == "__main__":
-  s = socket.socket()         # Create a socket object
-  host = "127.0.0.1"     # Get local machine name
-  port = 5555                 # Reserve a port for your service.
-  s.bind((host, port))        # Bind to the port
+    s = socket.socket()  # Create a socket object
+    host = "127.0.0.1"  # Get local machine name
+    port = 9999  # Reserve a port for your service.
+    s.bind((host, port))  # Bind to the port
 
-  print("Listening on port: %s" % str(port))
+    print("Listening on port: %s" % str(port))
 
-  s.listen(5)                 # Now wait for client connection.
-  c, addr = s.accept()        # Establish connection with client.
+    s.listen(5)  # Now wait for client connection.
+    c, addr = s.accept()  # Establish connection with client.
 
-  print( "Received request from: " + str( addr ) )
+    print("Received request from: " + str(addr))
 
-  sendData( c )
+    sendData(c)
